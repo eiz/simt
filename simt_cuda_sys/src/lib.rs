5abbfc3460625b49818b5dc32c2cc5453c827d6e
Mack Straight <eizneckam@gmail.com>
2023-05-10 22:31:59 -0700

    Error impl for CUresult

--- simt_cuda_sys/src/lib.rs
+++ simt_cuda_sys/src/lib.rs
@@ -3,5 +3,14 @@
 #![allow(non_snake_case)]
 #![allow(improper_ctypes)]
 
+use std::fmt::{Display, Formatter};
+
 // bindgen /usr/local/cuda-12.0/include/cuda.h --no-layout-tests --default-enum-style rust --dynamic-loading cuda --allowlist-function '^cu.*' > src/bindings.rs
 include!("bindings.rs");
+
+impl std::error::Error for CUresult {}
+impl Display for CUresult {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
