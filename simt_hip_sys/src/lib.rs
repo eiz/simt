5c3244ba3d22ea67c6d06f93b8364303940b29ef
Mack Straight <mack@str.rs>
2023-05-14 20:38:32 -0700

    hipError_t

--- simt_hip_sys/src/lib.rs
+++ simt_hip_sys/src/lib.rs
@@ -3,5 +3,14 @@
 #![allow(non_snake_case)]
 #![allow(improper_ctypes)]
 
+use std::fmt::{Formatter, Display};
+
 // bindgen /opt/rocm/include/hip/hip_runtime_api.h --no-layout-tests --default-enum-style rust --dynamic-loading hip --allowlist-function '^hip.*' -- -I/opt/rocm/include -D__HIP_PLATFORM_AMD__ > src/bindings.rs
 include!("bindings.rs");
+
+impl std::error::Error for hipError_t {}
+impl Display for hipError_t {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
